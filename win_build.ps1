d53c30477bcd6146518486075c6d38473026ab2a
Mediab0t <msmith@paloaltonetworks.com>
2021-02-26 15:59:24 +0000

    Update win_build.ps1

--- win_build.ps1
+++ win_build.ps1
@@ -1,12 +1,12 @@
 python -V
 pip install -r requirements.txt
-pyinstaller --onefile main_win.py
+pyinstaller --onefile main.py
 
 $date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss-"
 $rstring = (-join (((48..57)+(65..90)+(97..122)) * 80 | Get-Random -Count 16 |%{[char]$_}))
 $jstring = $date + $rstring + '.exe'
 
-mv .\dist\main_win.exe $jstring
+mv .\dist\main.exe $jstring
 ls
 
 Write-Host ''
